83383ab740ce18340d0dea75285abfa07b5a043d
estalaPaul <pestala495@gmail.com>
2020-08-22 04:32:30 -0600

    refactor: use env vars and fix spacing

--- php/getRepos.php
+++ php/getRepos.php
@@ -1,17 +1,19 @@
 <?php
-	if (isset($_GET["user"])) {	
-		$user = $_GET["user"];
-		$config = parse_ini_file('routetoconfigfile');
-		$header = $config['header'];
-		$headers = [
-			$header
-		];
-		$reposCh = curl_init();
-		curl_setopt($reposCh, CURLOPT_URL, 'https://api.github.com/users/'.$user.'/repos?per_page='.$config['reposCount'].'&sort='.$config['reposSort'].'&client_id='.$config['clientId'].'&client_secret='.$config['clientSecret']);
-		curl_setopt($reposCh, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($reposCh, CURLOPT_RETURNTRANSFER, true);
-		$repos = curl_exec($reposCh);
-		curl_close($reposCh);
-		echo $repos;
-	}
+    if (isset($_GET["user"])) {
+        $user = $_GET["user"];
+        $headers = ['User-Agent: estalaPaul'];
+        $reposCh = curl_init();
+        $url = sprintf(
+            'https://api.github.com/users/%s/repos?per_page=5&sort=created:%20asc&client_id=%s&client_secret=%s',
+            $user,
+            $_ENV['GH_CLIENT_ID'],
+            $_ENV['GH_CLIENT_SECRET']
+        );
+        curl_setopt($reposCh, CURLOPT_URL, $url);
+        curl_setopt($reposCh, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($reposCh, CURLOPT_RETURNTRANSFER, true);
+        $repos = curl_exec($reposCh);
+        curl_close($reposCh);
+        echo $repos;
+    }
 ?>
--- php/getUser.php
+++ php/getUser.php
@@ -1,17 +1,19 @@
 <?php
-	if (isset($_GET["user"])) {	
-		$user = $_GET["user"];
-		$config = parse_ini_file('routetoconfigfile');
-		$header = $config['header'];
-		$headers = [
-			$header
-		];
-		$userCh = curl_init();
+    if (isset($_GET["user"])) {
+        $user = $_GET["user"];
+        $headers = ['User-Agent: estalaPaul'];
+        $userCh = curl_init();
+        $url = sprintf(
+            'https://api.github.com/users/%s?client_id=%s&client_secret=%s',
+            $user,
+            $_ENV['GH_CLIENT_ID'],
+            $_ENV['GH_CLIENT_SECRET']
+        );
 
-		curl_setopt($userCh, CURLOPT_URL, 'https://api.github.com/users/'.$user.'?client_id='.$config['clientId'].'&client_secret='.$config['clientSecret']);
-		curl_setopt($userCh, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($userCh, CURLOPT_RETURNTRANSFER, true);
-		$profile = curl_exec($userCh);
-		echo $profile;
-	}
+        curl_setopt($userCh, CURLOPT_URL, $url);
+        curl_setopt($userCh, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($userCh, CURLOPT_RETURNTRANSFER, true);
+        $profile = curl_exec($userCh);
+        echo $profile;
+    }
 ?>
