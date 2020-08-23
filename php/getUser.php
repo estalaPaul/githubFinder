850d9c5495612ef8544c588cc93cf27697bc6dc0
estalaPaul <pestala495@gmail.com>
2020-08-23 03:29:16 -0600

    fix: remove query param auth

--- php/getUser.php
+++ php/getUser.php
@@ -3,16 +3,12 @@
         $user = $_GET["user"];
         $headers = ['User-Agent: estalaPaul'];
         $userCh = curl_init();
-        $url = sprintf(
-            'https://api.github.com/users/%s?client_id=%s&client_secret=%s',
-            $user,
-            $_ENV['GH_CLIENT_ID'],
-            $_ENV['GH_CLIENT_SECRET']
-        );
 
-        curl_setopt($userCh, CURLOPT_URL, $url);
+        curl_setopt($userCh, CURLOPT_URL, "https://api.github.com/users/$user");
         curl_setopt($userCh, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($userCh, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($userCh, CURLOPT_USERPWD, "{$_ENV['GH_CLIENT_ID']}:{$_ENV['GH_CLIENT_SECRET']}");
+        curl_setopt($userCh, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         $profile = curl_exec($userCh);
         curl_close($userCh);
         echo $profile;
