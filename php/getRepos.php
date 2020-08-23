1f5ec611ca2f880c85803d0b5645c9e657a83529
estalaPaul <pestala495@gmail.com>
2020-08-23 03:31:58 -0600

    fix: remove query param auth

--- php/getRepos.php
+++ php/getRepos.php
@@ -3,16 +3,12 @@
         $user = $_GET["user"];
         $headers = ['User-Agent: estalaPaul'];
         $reposCh = curl_init();
-        $url = sprintf(
-            'https://api.github.com/users/%s/repos?per_page=5&sort=created:%%20asc&client_id=%s&client_secret=%s',
-            $user,
-            $_ENV['GH_CLIENT_ID'],
-            $_ENV['GH_CLIENT_SECRET']
-        );
 
-        curl_setopt($reposCh, CURLOPT_URL, $url);
+        curl_setopt($reposCh, CURLOPT_URL, "https://api.github.com/users/$user/repos?per_page=5&sort=created:%20asc");
         curl_setopt($reposCh, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($reposCh, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($userCh, CURLOPT_USERPWD, "{$_ENV['GH_CLIENT_ID']}:{$_ENV['GH_CLIENT_SECRET']}");
+        curl_setopt($userCh, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         $repos = curl_exec($reposCh);
         curl_close($reposCh);
         echo $repos;
